f9d946a1a7b353535eef4d6976ba8fbf58c1fcd2
nkola1125 <maccybeer@gmail.com>
2025-12-02 13:51:40 +0300

    Update SESSION_ID in setting.js

--- setting.js
+++ setting.js
@@ -4,7 +4,7 @@ module.exports = {
   BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
   OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
   OWNER_NUMBER: '263714757857', // add your phone number
-  SESSION_ID: '', // add your session ID
+  SESSION_ID: 'a2ea31bca7451a5a358a82462d8b8ebf96374032', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
